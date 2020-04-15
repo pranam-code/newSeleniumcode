39e0e70339226f87b343a57f5203bc36896b1806
pranam-code <sonalipranam@gmail.com>
2020-04-14 23:16:35 -0400

    press f10 to right click

--- Right_click.py
+++ Right_click.py
@@ -7,6 +7,7 @@ driver = webdriver.Chrome(
     executable_path="C:/Users/manoj/eclipse/java-2019-12/eclipse/chromedriver_win32/chromedriver.exe")
 driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html")
 driver.maximize_window()
+##press f10 to right click on the browser
 button=driver.find_element_by_xpath("/html/body/div/section/div/div/div/p/span")
 action=ActionChains(driver)
 action.context_click(button).perform()
