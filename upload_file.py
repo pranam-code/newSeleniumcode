26ceb60e5fac05853a730c0ad687add16c9d68ed
pranam-code <sonalipranam@gmail.com>
2020-04-15 21:22:39 -0400

    upload files

--- upload_file.py
+++ upload_file.py
@@ -6,4 +6,4 @@ driver = webdriver.Chrome(
 driver.get("http://testautomationpractice.blogspot.com/")
 driver.maximize_window()
 driver.switch_to.frame(0)
-driver.find_element_by_id("RESULT_FileUpload-10").send_keys("C:/My_programs/images (1).jfif")
+driver.find_element_by_id("RESULT_FileUpload-10").send_keys("C:/Users/manoj/Desktop/download.png")
