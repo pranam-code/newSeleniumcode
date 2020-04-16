ace6778909f31c934d93b9c8f747d5104c500882
pranam-code <sonalipranam@gmail.com>
2020-04-15 23:28:41 -0400

    read excel file-sheet

---
+++ read_excel.py
@@ -0,0 +1,14 @@
+import openpyxl
+path="C:/My_programs/Book1.xlsx"
+workbook=openpyxl.load_workbook(path)
+sheet=workbook.active
+#sheet=workbook.get_sheet_by_name("sheet1")
+rows=sheet.max_row
+cols=sheet.max_column
+print(rows)
+print(cols)
+for r in range(1,rows+1):
+    for c in range(1,cols+1):
+        print(sheet.cell(row=r,column=c).value,end="      ")
+    print()
+
